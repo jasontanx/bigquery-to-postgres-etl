4b68f8a0073563f2ef0e8785f7bcf373e96ead9d
jason <jasontan9178@gmail.com>
2023-03-26 10:53:50 +0800

    sql file

--- main.py
+++ main.py
@@ -16,20 +16,6 @@ from ingest import ingest
 from sqlalchemy import create_engine
 
 
-def connection_dtl(connection_json):
-    """
-    Get SFTP connection details
-    """
-    with open(connection_json) as json_file:
-        json_decoded = json.load(json_file)
-
-    server = json_decoded["server"]
-    port = json_decoded["port"]
-    username = json_decoded["username"]
-    password = json_decoded["password"]
-    return server, port, username, password
-
-
 def main(params) -> None:
     user = params.user
     password = params.password
@@ -38,22 +24,15 @@ def main(params) -> None:
     db = params.db
     table_name = params.table_name
 
-    # temp_path = 'extracted'
-
-    # if not os.path.exists(temp_path):
-    #     os.makedirs(temp_path)
-
     date_fmt = datetime.now().strftime("%Y%m%d") + ".csv"
     file_name = "social_demo_2021_data" + date_fmt
 
     extract_data_from_query(file_name) #args.project_id, args.query, args.output_path
 
-
     engine = create_engine(f'postgresql://{user}:{password}@{host}:{port}/{db}')
     engine.connect()
     print('engine.connect')
     ingest(file_name, table_name, engine)
-
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser(description='Ingest CSV data to Postgres')
@@ -65,4 +44,3 @@ if __name__ == '__main__':
     parser.add_argument('--table_name', required=True, help='name of the table where we will write the results to')
     args = parser.parse_args()
     main(args)
-
---
+++ sql/query.sql
@@ -0,0 +1,3 @@
+SELECT * FROM `myfirstproject-364809.socio_demographic.salaries_ethnicity_sex` 
+WHERE year = '2021'
+LIMIT 1000;
